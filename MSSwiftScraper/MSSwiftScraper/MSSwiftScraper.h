4b4eef035f9b7f99e2d78acc008cab9251a234b3
Managam Silalahi <managam.silalahi@gmail.com>
2017-12-04 18:57:36 +0700

    initial cocoatouch library project

---
+++ MSSwiftScraper/MSSwiftScraper/MSSwiftScraper.h
@@ -0,0 +1,19 @@
+//
+//  MSSwiftScraper.h
+//  MSSwiftScraper
+//
+//  Created by Managam Silalahi on 04/12/17.
+//  Copyright © 2017 Managam Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MSSwiftScraper.
+FOUNDATION_EXPORT double MSSwiftScraperVersionNumber;
+
+//! Project version string for MSSwiftScraper.
+FOUNDATION_EXPORT const unsigned char MSSwiftScraperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSSwiftScraper/PublicHeader.h>
+
+
